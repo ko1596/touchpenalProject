5625fd78e49a3e6bd594e08756f90fdd1b636485
ko1596 <ko89022189@gmail.com>
2021-10-05 15:43:57 +0800

    buffer

--- frame.py
+++ frame.py
@@ -13,7 +13,7 @@ import sys
 SCREEN_WEIDTH = 1600
 SCREEN_HEIGHT = 1200
 BLOCK_WRIDTH = 200
-BLOCK_HEIGHT = 200
+BLOCK_HEIGHT = 230
 b,g,r,a = 0,0,0,0
 
 img = np.zeros((SCREEN_HEIGHT,SCREEN_WEIDTH,3),np.uint8)
@@ -22,11 +22,19 @@ cv2.rectangle(img, (200, 100), (1500, 1100), (255, 255, 255), -1)
 x = 0
 y = 0
 for i in sys.argv[1:]:
-    if int(i) > 40000:
-        bright = 0
+    if int(i) > 35000 and int(i) < 200:
+        red = 0
+        green = 0
+    elif int(i) > 5000 and int(i) < 50000:
+        red = 255
+        green = 0
+    elif int(i) > 200 and int(i) < 5000:
+        red = 0
+        green = 255
     else:
-        bright = int(i)/40000*255
-    cv2.rectangle(img, (250 + x*BLOCK_WRIDTH+10, 150 + y*BLOCK_HEIGHT+10), (250 + x*BLOCK_WRIDTH + BLOCK_WRIDTH, 150 + y*BLOCK_HEIGHT + BLOCK_HEIGHT), (0, 0, bright), -1)
+        red = 0
+        green = 0
+    cv2.rectangle(img, (250 + x*BLOCK_WRIDTH+10, 120 + y*BLOCK_HEIGHT+10), (250 + x*BLOCK_WRIDTH + BLOCK_WRIDTH, 120 + y*BLOCK_HEIGHT + BLOCK_HEIGHT), (0, green, red), -1)
     x = x + 1
     if x == 6:
         x = 0
@@ -34,7 +42,7 @@ for i in sys.argv[1:]:
     
 
 fontpath = "./NotoSansCJKtc-Light.otf"
-font = ImageFont.truetype(fontpath, 200)
+font = ImageFont.truetype(fontpath, 200) 
 img_pil = Image.fromarray(img)
 draw = ImageDraw.Draw(img_pil)
 # draw.text((500, 500),  "aaaa", font = font, fill = (0, 0, 0, a))
